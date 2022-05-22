a3f7b221968e815391e5cf1a3c8fc10c6df8ade3
Thais Rodrigues Neubauer <99596044+trodriguesneubauer@users.noreply.github.com>
2022-05-22 13:15:06 -0300

    Ajustando ordem das instruções do js para alinhar com o tutorial- etapa 5

--- SEMANA_05/02_TUTORIAL/backend/app_51.js
+++ SEMANA_05/02_TUTORIAL/backend/app_51.js
@@ -1,14 +1,14 @@
 const express = require('express'); 
-const hostname = '127.0.0.1';
+const app = express();
 
+const hostname = '127.0.0.1';
 const port = 3051;
 const sqlite3 = require('sqlite3').verbose(); 
-const app = express();
-const DBPATH = 'dbUser.db';
+const DBPATH = 'dbUser.db'; 
 
 app.use(express.static("../frontend/"));
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/user1', (req, res) => {
   res.statusCode = 200;
@@ -17,18 +17,29 @@ app.get('/user1', (req, res) => {
   var db = new sqlite3.Database(DBPATH);
   var sql = 'SELECT * FROM tbUser WHERE userId = 1';
   db.get(sql, [], (err, row) => {
-    if (err) {
+      if (err) {
       throw err;
-    }
-    //res.write("<h1> Teste do banco de dados</h1>") 
-    //res.write("<h4> Informacoes do usuario cujo id = 1: </h2>") 
-	  //res.write("title = " + row.title); 
-    //res.write("<br />completed = " + row.completed); 
-	  res.json(row);
-   });
+      }
+      res.json(row);
+      /*O código comentado abaixo é o que foi utilizado na etapa 1.
+        
+        Nele, ao invés a resposta (resultado da execução da cláusula
+        sql - variável "sql" deste código) ser fornecida via json,
+        como fazemos na linha acima deste comentário, uma página html
+        era construída e o resultado era diretamente inserido no html, 
+        como pode ser observado na última do código comentado abaixo.
+        
+        Observe ainda que essa forma de escrita do resultado em html, 
+        é necessário indicar que o conteúdo é do tipo html -> comentário
+        da linha 15 deste arquivo js*/
+    
+      //res.write("<h1> Teste do banco de dados</h1>") 
+      //res.write("<h4> Informacoes do usuario cujo id = 1: </h2>") 
+      //res.write("title = " + row.title); 
+      //res.write("<br />completed = " + row.completed); 
+  });
 });
 
-/* Inicia o servidor */
 app.listen(port, hostname, () => {
-  console.log(`BD server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
--- SEMANA_05/02_TUTORIAL/backend/app_52.js
+++ SEMANA_05/02_TUTORIAL/backend/app_52.js
@@ -1,54 +1,49 @@
 const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3052;
+const sqlite3 = require('sqlite3').verbose(); 
+const DBPATH = 'dbUser.db'; 
+
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const hostname = '127.0.0.1';
-
-
-const port = 3052;
-const sqlite3 = require('sqlite3').verbose();
-const app = express();
-const DBPATH = 'dbUser.db';
-
 
 app.use(express.static("../frontend/"));
 
-
 app.use(express.json());
 
-
-/* Definição dos endpoints */
 app.get('/users', (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Access-Control-Allow-Origin', '*');
+  res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-	var db = new sqlite3.Database(DBPATH);
+  var db = new sqlite3.Database(DBPATH);
   var sql = 'SELECT * FROM tbUser ORDER BY title COLLATE NOCASE';
-	db.all(sql, [],  (err, rows ) => {
-		if (err) {
-		    throw err;
-		}
-		res.json(rows);
-	});
-	db.close();
+  db.all(sql, [],  (err, rows ) => {
+      if (err) {
+          throw err;
+      }
+      res.json(rows);
+  });
+  db.close();
 });
 
 
 app.post('/userinsert', urlencodedParser, (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
 
-	sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
-	var db = new sqlite3.Database(DBPATH); 
-	db.run(sql, [],  err => {
-		if (err) {
-		    throw err;
-		}
-	});
-	db.close();
-	res.end();
+  sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
+  var db = new sqlite3.Database(DBPATH); 
+  db.run(sql, [],  err => {
+      if (err) {
+          throw err;
+      }
+  });
+  db.close();
+  res.end();
 });
 
-/* Inicia o servidor */
 app.listen(port, hostname, () => {
-  console.log(`BD server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
