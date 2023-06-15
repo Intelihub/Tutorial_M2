6b0a5292065c627703c9853874a3e1e338b36983
FVasconcellos <84935638+ItsVasconcellos@users.noreply.github.com>
2023-06-15 12:45:05 +0000

    Update app.js and script.js

--- SEMANA_07/02_AUT_EST_ENTREGA/backend/app.js
+++ SEMANA_07/02_AUT_EST_ENTREGA/backend/app.js
@@ -1,11 +1,11 @@
-const express = require('express'); 
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const express = require("express");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const sqlite3 = require('sqlite3').verbose();
-const DBPATH = 'data/curriculo.db';
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "data/curriculo.db";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 const app = express();
 
@@ -17,165 +17,210 @@ app.use(express.static("./frontend/"));
 app.use(express.json());
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/cadastrados', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = 'SELECT * FROM info_pessoa ORDER BY nome COLLATE NOCASE';
-        db.all(sql, [],  (err, rows ) => {
-            if (err) {
-                throw err;
-            }
-            res.json(rows);
-        });
-        db.close(); // Fecha o banco
+app.get("/cadastrados", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = "SELECT * FROM info_pessoa ORDER BY nome COLLATE NOCASE";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+  db.close(); // Fecha o banco
 });
 
 // Insere um registro (é o C do CRUD - Create)
-app.post('/cadastra-dados', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    sql = "INSERT INTO inf	o_pessoa (nome, cep, telefone, endereco,descricao,cargo,foto,email) VALUES ('" + req.body.nome + "', " + req.body.cep + ", " + req.body.phone + ", '" + req.body.address + "', '" + req.body.description +"', '" + req.body.cargo + "', '" + req.body.photo + "', '" + req.body.email + "')";
-    console.log(sql);
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }	
-    });
-    res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
-    db.close(); // Fecha o banco
-    res.end();
+app.post("/cadastra-dados", urlencodedParser, (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  sql =
+    "INSERT INTO inf	o_pessoa (nome, cep, telefone, endereco,descricao,cargo,foto,email) VALUES ('" +
+    req.body.nome +
+    "', " +
+    req.body.cep +
+    ", " +
+    req.body.phone +
+    ", '" +
+    req.body.address +
+    "', '" +
+    req.body.description +
+    "', '" +
+    req.body.cargo +
+    "', '" +
+    req.body.photo +
+    "', '" +
+    req.body.email +
+    "')";
+  console.log(sql);
+  db.run(sql, [], (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+  db.close(); // Fecha o banco
+  res.end();
 });
 
 // Monta o formulário para o update (é o U do CRUD - Update)
-app.get('/atualiza-dados', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "SELECT * FROM info_pessoa WHERE id_pessoa ="+ req.query.id_pessoa;
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-    db.close(); // Fecha o banco
+app.get("/atualiza-dados", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  sql = "SELECT * FROM info_pessoa WHERE id_pessoa =" + req.query.id_pessoa;
+  console.log(sql);
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+  db.close(); // Fecha o banco
 });
 
 // Atualiza um registro (é o U do CRUD - Update)
-app.post('/atualiza-dados', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "UPDATE info_pessoa SET nome='" + req.body.nome + "',cep = " + req.body.cep + ", telefone =" + req.body.phone + ", endereco ='" + req.body.address + "', descricao ='" + req.body.description +"', cargo ='" + req.body.cargo + "', foto = '" + req.body.photo + "', email = '" + req.body.email + "' WHERE id_pessoa ='" + req.body.id_pessoa + "'";
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }
-        res.end();
-    });
-    res.write('<p>USUARIO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
-    db.close(); // Fecha o banco
+app.post("/atualiza-dados", urlencodedParser, (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  sql =
+    "UPDATE info_pessoa SET nome='" +
+    req.body.nome +
+    "',cep = " +
+    req.body.cep +
+    ", telefone =" +
+    req.body.phone +
+    ", endereco ='" +
+    req.body.address +
+    "', descricao ='" +
+    req.body.description +
+    "', cargo ='" +
+    req.body.cargo +
+    "', foto = '" +
+    req.body.photo +
+    "', email = '" +
+    req.body.email +
+    "' WHERE id_pessoa ='" +
+    req.body.id_pessoa +
+    "'";
+  console.log(sql);
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  db.run(sql, [], (err) => {
+    if (err) {
+      throw err;
+    }
+    res.end();
+  });
+  res.write('<p>USUARIO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+  db.close(); // Fecha o banco
 });
 
 // Exclui um registro (é o D do CRUD - Delete)
-app.get('/remove-cadastro', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    sql = "DELETE FROM info_pessoa WHERE id_pessoa='" + req.query.id_pessoa + "'";
-    console.log(sql);
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }
-        res.write('<p>USUARIO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
-        res.end();
-    });
-    db.close(); // Fecha o banco
+app.get("/remove-cadastro", urlencodedParser, (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  sql = "DELETE FROM info_pessoa WHERE id_pessoa='" + req.query.id_pessoa + "'";
+  console.log(sql);
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  db.run(sql, [], (err) => {
+    if (err) {
+      throw err;
+    }
+    res.write('<p>USUARIO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+    res.end();
+  });
+  db.close(); // Fecha o banco
 });
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/formacao', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = `SELECT  info_pessoa.nome, formacao.id_formacao, formacao.ano_inicio, formacao.ano_fim, formacao.nome_curso, formacao.descricao FROM formacao 
-    INNER JOIN info_pessoa ON formacao.id_pessoa = info_pessoa.id_pessoa WHERE formacao.id_pessoa = ` + req.query.id_pessoa + ` ORDER BY formacao.nome_curso COLLATE NOCASE`;
-    console.log(sql);
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-        console.log(rows);
-    });
+app.get("/formacao", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql =
+    `SELECT  info_pessoa.nome, formacao.id_formacao, formacao.ano_inicio, formacao.ano_fim, formacao.nome_curso, formacao.descricao FROM formacao 
+    INNER JOIN info_pessoa ON formacao.id_pessoa = info_pessoa.id_pessoa WHERE formacao.id_pessoa = ` +
+    req.query.id_pessoa +
+    ` ORDER BY formacao.nome_curso COLLATE NOCASE`;
+  console.log(sql);
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+    console.log(rows);
+  });
 
-    db.close(); // Fecha o banco
+  db.close(); // Fecha o banco
 });
-
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/experiencia', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = `SELECT  info_pessoa.nome, experiencia.id_experiencia, experiencia.nome_empresa, experiencia.ano_inicio, experiencia.ano_fim, experiencia.cargo, experiencia.descricao FROM experiencia 
-    INNER JOIN info_pessoa ON experiencia.id_pessoa = info_pessoa.id_pessoa WHERE experiencia.id_pessoa = ` + req.query.id_pessoa + ` ORDER BY experiencia.nome_empresa COLLATE NOCASE`;
-    console.log(sql);
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-        console.log(rows);
-    });
-    db.close(); // Fecha o banco
+app.get("/experiencia", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql =
+    `SELECT  info_pessoa.nome, experiencia.id_experiencia, experiencia.nome_empresa, experiencia.ano_inicio, experiencia.ano_fim, experiencia.cargo, experiencia.descricao FROM experiencia 
+    INNER JOIN info_pessoa ON experiencia.id_pessoa = info_pessoa.id_pessoa WHERE experiencia.id_pessoa = ` +
+    req.query.id_pessoa +
+    ` ORDER BY experiencia.nome_empresa COLLATE NOCASE`;
+  console.log(sql);
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+    console.log(rows);
+  });
+  db.close(); // Fecha o banco
 });
-
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/habilidades', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = `SELECT  info_pessoa.nome, habilidades.id_habilidade, habilidades.nome, habilidades.qualidade FROM habilidades 
-    INNER JOIN info_pessoa ON habilidades.id_pessoa = info_pessoa.id_pessoa WHERE habilidades.id_pessoa = ` + req.query.id_pessoa + ` ORDER BY habilidades.nome COLLATE NOCASE`;
-    console.log(sql);
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-        console.log(rows);
-    });
+app.get("/habilidades", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql =
+    `SELECT  info_pessoa.nome, habilidades.id_habilidade, habilidades.nome, habilidades.qualidade FROM habilidades 
+    INNER JOIN info_pessoa ON habilidades.id_pessoa = info_pessoa.id_pessoa WHERE habilidades.id_pessoa = ` +
+    req.query.id_pessoa +
+    ` ORDER BY habilidades.nome COLLATE NOCASE`;
+  console.log(sql);
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+    console.log(rows);
+  });
 
-    db.close(); // Fecha o banco
+  db.close(); // Fecha o banco
 });
-
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/realizacao', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = `SELECT  info_pessoa.nome, realizacao.id_realizacao, realizacao.titulo,realizacao.ano, realizacao.descricao FROM realizacao 
-    INNER JOIN info_pessoa ON realizacao.id_pessoa = info_pessoa.id_pessoa WHERE realizacao.id_pessoa = ` + req.query.id_pessoa + ` ORDER BY realizacao.ano COLLATE NOCASE`;
-    console.log(sql);
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-        console.log(rows);
-    });
+app.get("/realizacao", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql =
+    `SELECT  info_pessoa.nome, realizacao.id_realizacao, realizacao.titulo,realizacao.ano, realizacao.descricao FROM realizacao 
+    INNER JOIN info_pessoa ON realizacao.id_pessoa = info_pessoa.id_pessoa WHERE realizacao.id_pessoa = ` +
+    req.query.id_pessoa +
+    ` ORDER BY realizacao.ano COLLATE NOCASE`;
+  console.log(sql);
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+    console.log(rows);
+  });
 
-    db.close(); // Fecha o banco
+  db.close(); // Fecha o banco
 });
 
-app.listen(port, hostname, () => {
-console.log(`Servidor rodando em http://${hostname}:${port}/`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando!`);
 });
--- SEMANA_07/02_AUT_EST_ENTREGA/frontend/script.js
+++ SEMANA_07/02_AUT_EST_ENTREGA/frontend/script.js
@@ -1,115 +1,137 @@
-var api = 'http://127.0.0.1:3000';
+var api = "";
 var id = 3;
 $(document).ready(() => {
-    perfil.list();
-    habilidades.list();
-    formacao.list();
-    experiencia.list();
-    realizacao.list();
+  perfil.list();
+  habilidades.list();
+  formacao.list();
+  experiencia.list();
+  realizacao.list();
 });
 
 var perfil = {
-    list() {
-        $.ajax({
-            url: api + '/cadastrados',
-            type: 'GET',
-            success: data => {
-                const imageElement = document.createElement('img');
-                let imageUrl;
-                let descricao = '';
-                data.forEach(element => {
-
-                    id = `${element.id_pessoa}`;
-                    imageUrl = `${element.foto}`;
-                    descricao += " " + `${element.descricao}`;
-                })
-                imageElement.src = imageUrl;
-                const container = document.getElementById('logo');
-                container.appendChild(imageElement);
-                document.getElementById("descricao").innerHTML += descricao;
-                }
-            }
-        );   
-    }
+  list() {
+    $.ajax({
+      url: api + "/cadastrados",
+      type: "GET",
+      success: (data) => {
+        const imageElement = document.createElement("img");
+        let imageUrl;
+        let descricao = "";
+        data.forEach((element) => {
+          id = `${element.id_pessoa}`;
+          imageUrl = `${element.foto}`;
+          descricao += " " + `${element.descricao}`;
+        });
+        imageElement.src = imageUrl;
+        const container = document.getElementById("logo");
+        container.appendChild(imageElement);
+        document.getElementById("descricao").innerHTML += descricao;
+      },
+    });
+  },
 };
 
 var habilidades = {
-    list(){
-        $.ajax({
-            url: api + '/habilidades?id_pessoa=' + id,
-            type: 'GET',
-            success: data => {
-                let habilidades = data;
-                let hab = '';
-                let int = '';
-                habilidades.map(function(habilidades) {
-                hab += "<br><h3>" + `${habilidades.nome}` + "</h3>";
-                int = `${habilidades.qualidade}`;
-                });
-                const tag = document.getElementById('stars'); // Retrieve the tag element
-                document.getElementById("habilidades").innerHTML += hab;
-                const content = "<img src='star.png' style='margin: 0 0;'>";
-                let multipliedContent = ''; // Variable to store the multiplied content
-                const separator = ' '; 
-                for (let i = 0; i < int; i++) {
-                multipliedContent += content + separator; // Concatenate the tag's inner HTML content to the multipliedContent variable
-                }
-                tag.innerHTML = multipliedContent;
-            }
-        })
-    }
-}
+  list() {
+    $.ajax({
+      url: api + "/habilidades?id_pessoa=" + id,
+      type: "GET",
+      success: (data) => {
+        let habilidades = data;
+        let hab = "";
+        let int = "";
+        habilidades.map(function (habilidades) {
+          hab += "<br><h3>" + `${habilidades.nome}` + "</h3>";
+          int = `${habilidades.qualidade}`;
+        });
+        const tag = document.getElementById("stars"); // Retrieve the tag element
+        document.getElementById("habilidades").innerHTML += hab;
+        const content = "<img src='star.png' style='margin: 0 0;'>";
+        let multipliedContent = ""; // Variable to store the multiplied content
+        const separator = " ";
+        for (let i = 0; i < int; i++) {
+          multipliedContent += content + separator; // Concatenate the tag's inner HTML content to the multipliedContent variable
+        }
+        tag.innerHTML = multipliedContent;
+      },
+    });
+  },
+};
 
 var formacao = {
-    list(){
-        $.ajax({
-            url: api + "/formacao?id_pessoa=" + id,
-            type: 'GET',
-            success: data => {  console.log("teste");
-            let formacao = data;
-            let forms = '';
-            formacao.map(function(formacao) {
-              forms += "<br><h3>" + `${formacao.nome_curso}` +"</h3> " + `${formacao.ano_inicio}` + " - " + `${formacao.ano_fim}` + "<br><p>" + `${formacao.descricao}` +"</p>" ;
-            });
-            console.log(forms);
-            document.getElementById("formacao").innerHTML += forms;
-        }})
-    }
-}
-
-var experiencia ={
-    list(){
-        $.ajax({
-            url: api + "/experiencia?id_pessoa=" + id,
-            type: 'GET',
-            success: data => {  
-                let experiencia = data;
-                let exp = '';
-                experiencia.map(function(experiencia) {
-                    exp += "<br><h3>" + `${experiencia.cargo}` +"</h3> " + `${experiencia.nome_empresa}` + " " + `${experiencia.ano_inicio}` + " - " //+ `${formacao.ano_fim}` +  +"<br><p>" + `${experiencia.descricao}` +"</p>" ;
-                });
-                console.log(exp);
-                document.getElementById("experiencia").innerHTML += exp;
-            }
-        })
-    }
-}
-
-
-var realizacao ={
-    list(){
+  list() {
     $.ajax({
-        url: api + "/realizacao?id_pessoa=" + id,
-        type: 'GET',  
-        success: data=>{
-            let realizacao = data;
-            let rel = '';
-            realizacao.map(function(realizacao) {
-            rel += "<br><h3>" + `${realizacao.titulo}` +"</h3>" + `${realizacao.ano}` + "<br> <p>" + `${realizacao.descricao}` +"</p>" ;
-            });
+      url: api + "/formacao?id_pessoa=" + id,
+      type: "GET",
+      success: (data) => {
+        console.log("teste");
+        let formacao = data;
+        let forms = "";
+        formacao.map(function (formacao) {
+          forms +=
+            "<br><h3>" +
+            `${formacao.nome_curso}` +
+            "</h3> " +
+            `${formacao.ano_inicio}` +
+            " - " +
+            `${formacao.ano_fim}` +
+            "<br><p>" +
+            `${formacao.descricao}` +
+            "</p>";
+        });
+        console.log(forms);
+        document.getElementById("formacao").innerHTML += forms;
+      },
+    });
+  },
+};
+
+var experiencia = {
+  list() {
+    $.ajax({
+      url: api + "/experiencia?id_pessoa=" + id,
+      type: "GET",
+      success: (data) => {
+        let experiencia = data;
+        let exp = "";
+        experiencia.map(function (experiencia) {
+          exp +=
+            "<br><h3>" +
+            `${experiencia.cargo}` +
+            "</h3> " +
+            `${experiencia.nome_empresa}` +
+            " " +
+            `${experiencia.ano_inicio}` +
+            " - "; //+ `${formacao.ano_fim}` +  +"<br><p>" + `${experiencia.descricao}` +"</p>" ;
+        });
+        console.log(exp);
+        document.getElementById("experiencia").innerHTML += exp;
+      },
+    });
+  },
+};
+
+var realizacao = {
+  list() {
+    $.ajax({
+      url: api + "/realizacao?id_pessoa=" + id,
+      type: "GET",
+      success: (data) => {
+        let realizacao = data;
+        let rel = "";
+        realizacao.map(function (realizacao) {
+          rel +=
+            "<br><h3>" +
+            `${realizacao.titulo}` +
+            "</h3>" +
+            `${realizacao.ano}` +
+            "<br> <p>" +
+            `${realizacao.descricao}` +
+            "</p>";
+        });
         console.log(rel);
         document.getElementById("realizacao").innerHTML += rel;
-        
-      }})
-    }
-}
+      },
+    });
+  },
+};
